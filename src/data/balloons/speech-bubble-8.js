c0dc76a3eb85c10de8d1f843d8a7bb2661212c50
Dimitry Ivanov <2@ivanoff.org.ua>
2019-12-13 10:46:56 +0000

    add more icons

---
+++ src/data/balloons/speech-bubble-8.js
@@ -0,0 +1,12 @@
+module.exports = `
+<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 71.015 71.015" style="enable-background:new 0 0 71.015 71.015;" xml:space="preserve">
+<g>
+	<circle cx="52.46" cy="27.353" r="4.206"/>
+	<circle cx="35.306" cy="27.353" r="4.206"/>
+	<circle cx="18.151" cy="27.353" r="4.206"/>
+	<path d="M13.223,67.769l0.022-15.916L0,51.728V3.247h71.015v48.5H36.448L13.223,67.769z M4,47.765l13.25,0.125l-0.017,12.252
+		l17.968-12.396h31.813v-40.5H4V47.765z"/>
+</g>
+</svg>
+`
